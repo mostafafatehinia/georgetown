1aeb20498b46977569c5847bcf18d47f327c9fd1
mo3tafa <mostafafatehinia@gmail.com>
2022-01-09 15:27:15 +0330

    achiements title style changed.

--- src/components/Achievement/Achievements.tsx
+++ src/components/Achievement/Achievements.tsx
@@ -4,9 +4,7 @@ import { Calculator, Chimistry, Computer, Earth, Mathematics } from "../Icons";
 export const Achievements = () => {
 	return (
 		<div>
-			<p className='text-center text-2xl drop-shadow-lg font-bold mt-8 border-t border-[#627b9b] text-[#627b9b] mx-12 pt-4'>
-				Achiements
-			</p>
+			<p className='text-center text-2xl drop-shadow-lg font-bold mt-8  text-[#627b9b] mx-12 pt-4'>Achiements</p>
 			<div className='flex flex-wrap capitalize justify-around my-4 px-16 text-sm font-bold md:text-xl'>
 				<Achievement>
 					<Computer />
